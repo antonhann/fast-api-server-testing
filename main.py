3a1a7e344404c45070dec6681a9d65ce2a4e5869
antonha016@gmail.com <antonhan@tonpc.>
2024-10-18 19:34:44 -0400

    added full runthrough of the fast api

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from enum import Enum
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel 
+
+from pydantic import BaseModel
+
+from fastapi import FastAPI, HTTPException, Path, Query
 
 app = FastAPI()
 
 
 class Category(Enum):
-    TOOLS = 'tools'
-    CONSUMABLES = 'consumables'
+    TOOLS = "tools"
+    CONSUMABLES = "consumables"
 
 
 class Item(BaseModel):
@@ -24,22 +26,25 @@ items = {
     2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
 }
 
-#to run the server uvicorn main:app --reload
-# FastAPI handles JSON serialization and deserialization for us.
-# We can simply use built-in python and Pydantic types, in this case dict[int, Item].
+
 @app.get("/")
 def index() -> dict[str, dict[int, Item]]:
     return {"items": items}
 
+
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> Item:
+def query_item_by_id(item_id: int) -> Item:
     if item_id not in items:
-        # FastAPI will automatically return a 404 response with the message "Item not found".
-        raise HTTPException(status_code=404, detail="Item not found")
-        
+        HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+
     return items[item_id]
 
-Selection = dict[str, str | int | float | Category | None]
+
+Selection = dict[
+    str, str | int | float | Category | None
+]  # dictionary containing the user's query arguments
+
+
 @app.get("/items/")
 def query_item_by_parameters(
     name: str | None = None,
@@ -47,17 +52,67 @@ def query_item_by_parameters(
     count: int | None = None,
     category: Category | None = None,
 ) -> dict[str, Selection | list[Item]]:
-    def check_item(item: Item) -> bool:
+    def check_item(item: Item):
+        """Check if the item matches the query arguments from the outer scope."""
         return all(
-            (name is None or item.name == name,
-            price is None or item.price == price,
-            count is None or item.count == count,
-            category is None or item.category == category)
+            (
+                name is None or item.name == name,
+                price is None or item.price == price,
+                count is None or item.count != count,
+                category is None or item.category is category,
+            )
         )
+
     selection = [item for item in items.values() if check_item(item)]
     return {
         "query": {"name": name, "price": price, "count": count, "category": category},
         "selection": selection,
     }
 
-    
+
+@app.post("/")
+def add_item(item: Item) -> dict[str, Item]:
+    if item.id in items:
+        HTTPException(status_code=400, detail=f"Item with {item.id=} already exists.")
+
+    items[item.id] = item
+    return {"added": item}
+
+
+# We can place further restrictions on allowed arguments by using the Query and Path classes.
+# In this case we are setting a lower bound for valid values and a minimal and maximal length for the name.
+@app.put("/update/{item_id}")
+def update(
+    item_id: int = Path(ge=0),
+    name: str | None = Query(defaut=None, min_length=1, max_length=8),
+    price: float | None = Query(default=None, gt=0.0),
+    count: int | None = Query(default=None, ge=0),
+):
+    if item_id not in items:
+        HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+    if all(info is None for info in (name, price, count)):
+        raise HTTPException(
+            status_code=400, detail="No parameters provided for update."
+        )
+
+    item = items[item_id]
+    if name is not None:
+        item.name = name
+    if price is not None:
+        item.price = price
+    if count is not None:
+        item.count = count
+
+    return {"updated": item}
+
+
+@app.delete("/delete/{item_id}")
+def delete_item(item_id: int) -> dict[str, Item]:
+
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404, detail=f"Item with {item_id=} does not exist."
+        )
+
+    item = items.pop(item_id)
+    return {"deleted": item}
